0d6473a5764963d811cdc821cc162312ecf7893f
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-14 14:41:35 -0300

    Update calculadora de notas.php
    html

--- calculadora de notas.php
+++ calculadora de notas.php
@@ -1,27 +1,3 @@
-<?php
-session_start();
-
-// Função para calcular a média das notas
-function calcularMedia($notas) {
-  $soma = array_sum($notas);
-  $media = $soma / count($notas);
-  return $media;
-}
-
-// Verifica se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Obtém as notas do formulário
-  $notas = $_POST["notas"];
-
-  // Armazena as notas na sessão
-  $_SESSION["notas"] = $notas;
-
-  // Redireciona para a página de resultado
-  header("Location: resultado.php");
-  exit();
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
